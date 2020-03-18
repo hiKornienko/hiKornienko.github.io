492bbcf83b3f07bc6a8d8d699f2f495366f9ed04
Владислав Корнієнко <30860410+hiKornienko@users.noreply.github.com>
2020-03-18 16:40:13 +0200

    up

--- finaly/js/main.js
+++ finaly/js/main.js
@@ -43,6 +43,33 @@ for (let anchor of anchors) {
 /*** / плавный скроллинг ***/
 
 /*** отправка запроса ***/
+document.querySelector('.js-form-3').onclick = function() {
+  form(document.querySelector('.js-form-1'),document.querySelector('.js-form-2'),document.querySelector('.js-form-4'),document.querySelector('.js-form-5'),);
+}
+document.querySelector('.js-form2-3').onclick = function() {
+  form(document.querySelector('.js-form2-1'),document.querySelector('.js-form2-2'),document.querySelector('.js-form2-4'),document.querySelector('.js-form2-5'),);
+}
 
+function form(name,phone,ok,eror){
+  if (name.value.length > 1 && phone.value.length > 1) {
+    var xhr = new XMLHttpRequest();
+    var body = 'name=' + encodeURIComponent(name.value) + '&phone=' + encodeURIComponent(phone.value);
+    xhr.open("POST", '/mail.php', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        eror.innerHTML = '';
+        eror.style.display = 'none';
+        ok.style.display = 'block';
+        name.value = '';
+        phone.value = '';
+      }
+    }
+    xhr.send(body);
+  } else {
+    ok.style.display = 'none';
+    eror.style.display = 'block';
+  }
+}
 /*** / отправка запроса ***/
