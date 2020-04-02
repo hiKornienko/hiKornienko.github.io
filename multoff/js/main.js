67a49093f18dbf115c2cc7607b7dc4fc3e7ae747
Владислав Корнієнко <30860410+hiKornienko@users.noreply.github.com>
2020-04-02 11:42:52 +0300

    add

--- multoff/js/main.js
+++ multoff/js/main.js
@@ -1,16 +1,9 @@
 $('.owl-carousel').owlCarousel({
+    items:1,
     loop:true,
     margin:10,
     nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
+    autoplay:true,
+    autoplayTimeout:7000,
+    autoplaySpeed:1000
 })
